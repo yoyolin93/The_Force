6cf46ecfafcdd5f9631b71aff7cfd4cd9e0805f9
Avneesh Sarwate <avneeshsarwate@gmail.com>
2018-01-03 13:00:38 -0500

    more warp experimenting

--- forceCode/warpExperiments.glsl
+++ forceCode/warpExperiments.glsl
@@ -1,19 +1,35 @@
+float logi(float x){
+    return 1. / (1. + (1./exp(x)));
+}
+
+
+vec2 coordWarp(vec2 stN){
+    vec2 p1 = vec2(sinN(time * 1.3), cosN(time));
+    // p1 = vec2(0.25);
+    
+    vec2 p2 = vec2(sinN(time * 1.3 + 1.), cosN(time * 1.1 + 1.));
+    // p2 = vec2(0.75);
+    
+    
+    vec2 warp = stN;
+    
+    float rad = 1.;
+    
+    warp = length(p1 - stN) < rad ? mix(stN, p1, length(stN - p1))/rad  : warp;
+    
+    warp =length(p2 - stN) < rad ?  mix(warp, p2, 1. - length(stN - p2))/rad  : warp;
+    
+    return warp;
+}
+
 void main () {
 
     //the current pixel coordinate 
     vec2 stN = uvN();
-    vec2 mouseN = mouse.xy / resolution.xy;
-    vec2 p1 = vec2(sinN(time / 1.3), cosN(time));
-    
-    vec2 p2 = vec2(sinN(time / 1.3 + 1.), cosN(time * 1.1 + 1.));
-    
-    vec2 warp = stN;
-    
-    // warp = mix(stN, p1.xy, 1. / (length(abs(stN.xy - p1.xy)) + 1.1) );
-    
-    warp = mix(warp, p2.xy, 1. / (length(abs(stN.xy - p2.xy)) + 1.1) );
+    vec2 warp = coordWarp(stN);
 
     vec3 cam = texture2D(channel0, vec2(1.-warp.x, warp.y)).rgb;
+    // cam = swirl(time/3., warp);
     
     
     
