36ed4edefca3b8d6ce3f7da948da88ba287e3eb5
Avneesh Sarwate <avneeshsarwate@gmail.com>
2018-01-11 13:42:32 -0500

    updates

--- forceCode/touchscreenTrails.glsl
+++ forceCode/touchscreenTrails.glsl
@@ -1,3 +1,15 @@
+vec2 coordWarp(vec2 stN){
+    vec2 warp = stN;
+    
+    float rad = .2;
+    
+    for (float i = 0.0; i < 100.; i++) {
+        vec2 p = vec2(sinN(time/5. * rand(i) * 1.3 + i), cosN(time/5. * rand(i) * 1.1 + i));
+        warp = length(p - stN) <= rad ? mix(warp, p, 1. - length(stN - p)/rad)  : warp;
+    }
+    
+    return warp;
+}
 
 void main () {
 
@@ -5,12 +17,14 @@ void main () {
     vec2 stN = uvN();
     
     vec3 c;
-    float decay = 0.98;
+    float decay = 0.99;
     float feedback;
+    vec3 cam = texture2D(channel0, stN).xyz;
+    vec3 camWarp = texture2D(channel0, coordWarp(stN)).xyz;
     float lastFeedback = texture2D(backbuffer, vec2(stN.x, stN.y)).a; 
     bool condition = distance(touch1.xy, stN) < .1;
-    vec3 trail = length(touch2) > 0. ? touch2.xyz : white;
-    vec3 foreGround = length(touch3) > 0. ? touch3.xzy : black;
+    vec3 trail = length(touch2) > 0. ? cam * touch2.xyz : white;
+    vec3 foreGround = length(touch3) > 0. ? camWarp * touch3.xzy : black;
     
     // implement the trailing effectm using the alpha channel to track the state of decay 
     if(condition){
@@ -25,7 +39,7 @@ void main () {
     }
     else {
         feedback = lastFeedback * decay;
-        if(lastFeedback > 0.6) {
+        if(lastFeedback > 0.4) {
             c = mix(foreGround, trail, lastFeedback); 
         } else {
             feedback = 0.;
