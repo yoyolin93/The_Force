66bc7bc3399e191b9397d91df4cf3c80811e6574
Avneesh Sarwate <avneeshsarwate@gmail.com>
2018-02-10 23:20:19 -0500

    more parameter tweaking

--- forceCode/hexBlockAnimatedDiffTrail.glsl
+++ forceCode/hexBlockAnimatedDiffTrail.glsl
@@ -136,31 +136,8 @@ float hexDiffAvg(vec2 p, float numHex){
     return diff / 18.;
 }
 
-
-vec3 diffColor(float time2){
-    vec2 stN = uvN();
-    stN = rotate(vec2(0.5+sin(time2)*0.5, 0.5+cos(time2)*0.5), stN, sin(time2));
-    
-    vec2 segGrid = vec2(floor(stN.x*30.0 * sin(time2/7.)), floor(stN.y*30.0 * sin(time2/7.)));
-
-    vec2 xy;
-    float noiseVal = rand(stN)*sin(time2/7.) * 0.15;
-    if(mod(segGrid.x, 2.) == mod(segGrid.y, 2.)) xy = rotate(vec2(sinN(time2), cosN(time2)), stN.xy, time2 + noiseVal);
-    else xy = rotate(vec2(sinN(time2), cosN(time2)), stN.xy, - time2 - noiseVal);
-    
-    float section = floor(xy.x*30.0 * sin(time2/7.)); 
-    float tile = mod(section, 2.);
-
-    float section2 = floor(xy.y*30.0 * cos(time2/7.)); 
-    float tile2 = mod(section2, 2.);
-    float timeMod = time2 - (1. * floor(time2/1.)); 
-    
-    return vec3(tile, tile2, timeMod);
-}
-
-vec3 quant(vec3 num, float quantLevels){
-    vec3 roundPart = floor(fract(num*quantLevels)*2.);
-    return (floor(num*quantLevels)+roundPart)/quantLevels;
+vec2 trans(vec2 u, float scaleval){
+    return u*scaleval;
 }
 
 float quant(float num, float quantLevels){
@@ -168,81 +145,33 @@ float quant(float num, float quantLevels){
     return (floor(num*quantLevels)+roundPart)/quantLevels;
 }
 
-
-float wrap(float val, float low, float high){
-    if(val < low) return low + (low-val);
-    if(val > high) return high - (val - high);
-    return val;
-}
-
-float bound(float val, float lower, float upper){
-    return max(lower, min(upper, val));
-}
-
-bool boxHasColorDiff(vec2 xy, float boxSize, float diffThresh, float diffFrac){
-    float boxArea = boxSize * boxSize;
-    return true;
-}
-
-float block(float numBlocks, float quantLevel) {
+void main(){
     vec2 stN = uvN();
-    vec3 cam = texture2D(channel0, vec2(1.-stN.x, stN.y)).xyz; 
-    vec3 lumC = lum(cam);
-    vec2 res = gl_FragCoord.xy / stN;
-    vec2 blockSize = res.xy / numBlocks;
-    vec2 blockStart = floor(gl_FragCoord.xy / blockSize) * blockSize / res.xy;
-    float blockAvgLuma = 0.;
-    vec2 counter = blockStart;
+    vec3 cam = texture2D(channel0, vec2(1. - stN.x, stN.y)).xyz;
+    // Aspect correct screen coordinates.
+    float scaleval = 30. + sinN(time*3.) * 80.;
+    vec2 u = trans(uvN() , scaleval);
     
-    vec2 inc = vec2(1. / (numBlocks *100.));
-    for(float i = 0.; i < 10.; i += 1.){
-        for(float j = 0.; j < 10.; j += 1.){
-            blockAvgLuma += lum(texture2D(channel0, vec2(1.-counter.x, counter.y)).xyz).r;
-            counter += inc;
-        }
-    }
-    blockAvgLuma /= 100.;
+    float size = 1.;
+    vec2 codec = hex_to_pixel(pixel_to_hex(u, size), size);
+    vec2 hexV = pixel_to_hex(u, size);
+    vec2 diffVec = u - codec;
+    float diff = sqrt(dot(diffVec, diffVec));
     
-    return quant(blockAvgLuma, quantLevel);
-}
-
-void main () {
-    vec2 stN = uvN();
-    vec3 snap = texture2D(channel3, vec2(1. -stN.x, stN.y)).rgb;  
-    vec3 cam = texture2D(channel0, vec2(1. -stN.x, stN.y)).rgb;  
-    vec3 bb = texture2D(backbuffer, vec2(stN.x, stN.y)).rgb;
-    vec3 t1 = texture2D(channel1, vec2(1. -stN.x, stN.y)).rgb;  
-    vec3 col = diffColor(time/5.);
-
-    vec3 c;
-    float lastFeedback = texture2D(backbuffer, vec2(stN.x, stN.y)).a; 
-    float feedback; 
-    float decay = 0.97;
-    float blockColor = block(50.+ sinN(time/2.) * 40., 7.+sinN(time/1.5)*10.) + 0.01;
-    float lumBlend = 2.;
     
-    float hexDiff = hexDiffAvg(stN, 90.);
-    float pointDiff = colourDistance(cam, snap);
+    vec2 c = hexCenter2(stN,size);
+    float dist = distance(c/scaleval, u/scaleval)*scaleval;
     
-    if(hexDiff > 0.8){
-        if(lastFeedback < 1.) {
-            feedback = 1.;
-            c = col * pow(blockColor, lumBlend);
-        } else {
-            feedback = lastFeedback * decay;
-            c = mix(snap, bb, lastFeedback);
-        }
-    }
-    else {
-        feedback = lastFeedback * decay;
-        if(lastFeedback > 0.4) { //if you put this below 1 you might have never-fading shadows 
-            c = mix(t1, col * pow(blockColor, lumBlend), lastFeedback); //swap col for bb for glitchier effect
-        } else {
-            feedback = 0.;
-            c = t1;
-            //c = vec3(0.);
-        }
-    }
+    float sampled = inSampleSet(u, c) ? 1. : 0.;
     
-    gl_FragColor = vec4(c, feedback);
+    float radius = dist < .861 ? 1. : 0.;
+    
+    float avgLum = hexDiffAvg(stN, scaleval);
+    avgLum = avgLum > 0.2 ? avgLum-0.3 + rand(vec2(time, quant(stN.y+time/10., 100.)))/1.5 : 0.;
+    
+    float colorChoice = 0.;
+    vec3 col = avgLum == 0. ? vec3(avgLum) : colorChoice == 1. ? swirl(time/3., stN) : vec3(avgLum);
+    // Rough gamma correction.    
+ gl_FragColor = vec4(col, 1);
+    
 }
