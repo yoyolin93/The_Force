4658f0e81e4e148a3e45d34f0b4aca0879d2d98c
Avneesh Sarwate <avneeshsarwate@gmail.com>
2018-03-30 16:15:12 -0400

    more empress

--- forceCode/empressCamColor.glsl
+++ forceCode/empressCamColor.glsl
@@ -158,6 +158,12 @@ vec3 getArrayElem(vec3[10] arr, int ind){
     return vec3(0.);
 }
 
+// quantize and input number [0, 1] to quantLevels levels
+float quant(float num, float quantLevels){
+    float roundPart = floor(fract(num*quantLevels)*2.);
+    return (floor(num*quantLevels)+roundPart)/quantLevels;
+}
+
 vec3 quant(vec3 num, float quantLevels){
     vec3 roundPart = floor(fract(num*quantLevels)*2.);
     return (floor(num*quantLevels)+roundPart)/quantLevels;
@@ -166,6 +172,60 @@ vec3 quant(vec3 num, float quantLevels){
 vec2 quant(vec2 num, float quantLevels){
     vec2 roundPart = floor(fract(num*quantLevels)*2.);
     return (floor(num*quantLevels)+roundPart)/quantLevels;
+}
+
+/* bound a number to [low, high] and "wrap" the number back into the range
+if it exceeds the range on either side - 
+for example wrap(10, 1, 9) -> 8
+and wrap (-2, -1, 9) -> 0
+*/
+float wrap3(float val, float low, float high){
+    float range  = high - low;
+    if(val > high){
+        float dif = val-high;
+        float difMod = mod(dif, range);
+        float numWrap = dif/range - difMod;
+        if(mod(numWrap, 2.) == 0.){
+            return high - difMod;
+        } else {
+            return low + difMod;
+        }
+    }
+    if(val < low){
+        float dif = low-val;
+        float difMod = mod(dif, range);
+        float numWrap = dif/range - difMod;
+        if(mod(numWrap, 2.) == 0.){
+            return low + difMod;
+        } else {
+            return high - difMod;
+        }
+    }
+    return val;
+}
+vec2 wrap(vec2 val, float low, float high){
+    return vec2(wrap3(val.x, low, high), wrap3(val.y, low, high));
+}
+
+//slice the matrix up into columns and translate the individual columns in a moving wave
+vec2 columnWaves3(vec2 stN, float numColumns, float time2, float power){
+    return vec2(wrap3(stN.x + sin(time2*8.)*0.05 * power, 0., 1.), wrap3(stN.y + cos(quant(stN.x, numColumns)*5.+time2*2.)*0.22 * power, 0., 1.));
+}
+
+//slice the matrix up into rows and translate the individual rows in a moving wave
+vec2 rowWaves3(vec2 stN, float numColumns, float time2, float power){
+    return vec2(wrap3(stN.x + sin(quant(stN.y, numColumns)*5.+time2*2.)*0.22 * power, 0., 1.), wrap3(stN.y + cos(time2*8.)*0.05 * power, 0., 1.));
+}
+
+
+//iteratively apply the rowWave and columnWave functions repeatedly to 
+//granularly warp the grid
+vec2 rowColWave(vec2 stN, float div, float time2, float power){
+    for (int i = 0; i < 5; i++) {
+        stN = rowWaves3(stN, div, time2, power);
+        stN = columnWaves3(stN, div, time2, power);
+    }
+    return stN;
 }
 
 void main () {
@@ -181,7 +241,7 @@ void main () {
     
     vec3 camColor = texture2D(channel0, quant(stN, 140.)).xyz;
 
-    vec3 foreGround = texture2D(channel5, quant(camColor.xy, 3.)).xyz;
+    vec3 foreGround = texture2D(channel5, mix(stN, quant(camColor.xy, 30.), sinN(time))).xyz;
     
     vec2 warpStn = quant(coordWarp(stN), 10.);
 
@@ -221,5 +281,5 @@ void main () {
         }
     }
     
-    gl_FragColor = vec4(vec3(foreGround), feedback);
+    gl_FragColor = vec4(vec3(c), feedback);
 }
