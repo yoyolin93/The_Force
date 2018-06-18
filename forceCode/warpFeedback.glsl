81ba6a648402fb54b98a5884efbd33ffb8ca1d5b
Avneesh Sarwate <avneeshsarwate@gmail.com>
2018-06-18 14:05:27 +0100

    new shader

---
+++ forceCode/warpFeedback.glsl
@@ -0,0 +1,236 @@
+// quantize and input number [0, 1] to quantLevels levels
+float quant(float num, float quantLevels){
+    float roundPart = floor(fract(num*quantLevels)*2.);
+    return (floor(num*quantLevels)+roundPart)/quantLevels;
+}
+
+// same as above but for vectors, applying the quantization to each element
+vec3 quant(vec3 num, float quantLevels){
+    vec3 roundPart = floor(fract(num*quantLevels)*2.);
+    return (floor(num*quantLevels)+roundPart)/quantLevels;
+}
+
+// same as above but for vectors, applying the quantization to each element
+vec2 quant(vec2 num, float quantLevels){
+    vec2 roundPart = floor(fract(num*quantLevels)*2.);
+    return (floor(num*quantLevels)+roundPart)/quantLevels;
+}
+
+/* bound a number to [low, high] and "wrap" the number back into the range
+if it exceeds the range on either side - 
+for example wrap(10, 1, 9) -> 8
+and wrap (-2, -1, 9) -> 0
+*/
+float wrap3(float val, float low, float high){
+    float range  = high - low;
+    if(val > high){
+        float dif = val-high;
+        float difMod = mod(dif, range);
+        float numWrap = dif/range - difMod;
+        if(mod(numWrap, 2.) == 0.){
+            return high - difMod;
+        } else {
+            return low + difMod;
+        }
+    }
+    if(val < low){
+        float dif = low-val;
+        float difMod = mod(dif, range);
+        float numWrap = dif/range - difMod;
+        if(mod(numWrap, 2.) == 0.){
+            return low + difMod;
+        } else {
+            return high - difMod;
+        }
+    }
+    return val;
+}
+vec2 wrap(vec2 val, float low, float high){
+    return vec2(wrap3(val.x, low, high), wrap3(val.y, low, high));
+}
+
+//slice the matrix up into columns and translate the individual columns in a moving wave
+vec2 columnWaves3(vec2 stN, float numColumns, float time2, float power){
+    return vec2(wrap3(stN.x + sin(time2*8.)*0.05 * power, 0., 1.), wrap3(stN.y + cos(quant(stN.x, numColumns)*5.+time2*2.)*0.22 * power, 0., 1.));
+}
+
+//slice the matrix up into rows and translate the individual rows in a moving wave
+vec2 rowWaves3(vec2 stN, float numColumns, float time2, float power){
+    return vec2(wrap3(stN.x + sin(quant(stN.y, numColumns)*5.+time2*2.)*0.22 * power, 0., 1.), wrap3(stN.y + cos(time2*8.)*0.05 * power, 0., 1.));
+}
+
+
+//iteratively apply the rowWave and columnWave functions repeatedly to 
+//granularly warp the grid
+vec2 rowColWave(vec2 stN, float div, float time2, float power){
+    for (int i = 0; i < 10; i++) {
+        stN = rowWaves3(stN, div, time2, power);
+        stN = columnWaves3(stN, div, time2, power);
+    }
+    return stN;
+}
+
+vec4 colormap_hsv2rgb(float h, float s, float v) {
+    float r = v;
+    float g = v;
+    float b = v;
+    if (s > 0.0) {
+        h *= 6.0;
+        int i = int(h);
+        float f = h - float(i);
+        if (i == 1) {
+            r *= 1.0 - s * f;
+            b *= 1.0 - s;
+        } else if (i == 2) {
+            r *= 1.0 - s;
+            b *= 1.0 - s * (1.0 - f);
+        } else if (i == 3) {
+            r *= 1.0 - s;
+            g *= 1.0 - s * f;
+        } else if (i == 4) {
+            r *= 1.0 - s * (1.0 - f);
+            g *= 1.0 - s;
+        } else if (i == 5) {
+            g *= 1.0 - s;
+            b *= 1.0 - s * f;
+        } else {
+            g *= 1.0 - s * (1.0 - f);
+            b *= 1.0 - s;
+        }
+    }
+    return vec4(r, g, b, 1.0);
+}
+
+vec4 colormap(float x) {
+    float h = clamp(-7.44981265666511E-01 * x + 7.47965390904122E-01, 0.0, 1.0);
+    float s = 1.0;
+    float v = 1.0;
+    return colormap_hsv2rgb(h, s, v);
+}
+
+float colourDistance(vec3 e1, vec3 e2) {
+  float rmean = (e1.r + e2.r ) / 2.;
+  float r = e1.r - e2.r;
+  float g = e1.g - e2.g;
+  float b = e1.b - e2.b;
+  return sqrt((((512.+rmean)*r*r)/256.) + 4.*g*g + (((767.-rmean)*b*b)/256.));
+}
+
+vec4 circleSlice(vec2 stN, float t, float randw){
+    
+    //define several different timescales for the transformations
+    float t0, t1, t2, t3, t4, rw;
+    t0 = t/4.5;
+    t1 = t/2.1;
+    t2 = t/1.1;
+    t3 = t/0.93;
+    rw =  randw/290.; //a random walk value used to parameterize the rotation of the final frame
+    t4 = t;
+    
+    t1 = t1 / 2.;
+    t0 = t0 / 2.;
+    rw = rw / 2.;
+    float divx = sinN(t0) * 120.+10.;
+    float divy = cosN(t1) * 1400.+10.;
+    stN = stN * rotate(stN, vec2(0.5), rw);
+    vec2 trans2 = vec2(mod(floor(stN.y * divx), 2.) == 0. ? mod(stN.x + (t1 + rw)/4., 1.) : mod(stN.x - t1/4., 1.), 
+                       mod(floor(stN.x * divy), 2.) == 0. ? mod(stN.y + t1, 1.) : mod(stN.y - t1, 1.));
+    
+    
+    bool inStripe = false;
+    float dist = distance(trans2, vec2(0.5));
+
+
+    float numStripes = 20.;
+    float d = 0.05;
+    float stripeWidth =(0.5 - d) / numStripes;
+    for(int i = 0; i < 100; i++){
+        if(d < dist && dist < d + stripeWidth/2.) {
+            inStripe = inStripe || true;
+        } else {
+            inStripe = inStripe || false;
+        }
+        d = d + stripeWidth;
+        if(d > 0.5) break;
+    }
+    
+    vec4 c = !inStripe ? vec4(white, 1) : vec4(black, 0);
+    return c;
+    
+}
+
+vec3 coordWarp(vec2 stN, float t2){ 
+    vec2 warp = stN;
+    
+    float rad = .5;
+    
+    for (float i = 0.0; i < 20.; i++) {
+        vec2 p = vec2(sinN(t2* rand(i+1.) * 1.3 + i), cosN(t2 * rand(i+1.) * 1.1 + i));
+        warp = length(p - stN) <= rad ? mix(warp, p, 1. - length(stN - p)/rad)  : warp;
+    }
+    
+    return vec3(warp, distance(warp, stN));
+}
+
+bool multiBallCondition(vec2 stN, float t2){
+    
+    float rad = .01;
+    bool cond = false;
+    
+    for (int i = 0; i < 20; i++) {
+        float i_f = float(i);
+        vec2 p = vec2(sinN(t2 * rand(i_f+1.) * 1.3 + i_f), cosN(t2 * rand(i_f+1.) * 1.1 + i_f));
+        cond = cond || distance(stN, p) < rad;
+    }
+    
+    return cond;
+}
+
+void main () {
+
+    //the current pixel coordinate 
+    vec2 stN = uvN();
+
+    // vec4 c = circleSlice(stN, time/6., randWalk);   
+
+    
+    vec4 bb = texture2D(backbuffer, stN);
+    // c = quant(c, 2.);
+
+    // c = mix(c, bb, 0.4 + sinN(t0)*0.5);
+    
+    vec3 cc;
+    float decay = 0.97;
+    float feedback;
+    float lastFeedback = texture2D(backbuffer, coordWarp(stN, time/5.).xy).a;
+    // bool crazyCond = (circleSlice(stN, time/6., time + sinN(time*sinN(time)) *1.8).x - circleSlice(stN, (time-sinN(time))/6., time + sinN(time*sinN(time)) *1.8).x) == 0.;
+    bool condition = multiBallCondition(stN, time); 
+    vec3 trail = vec3(sinN(time/4.)*0.7); // swirl(time/5., trans2) * c.x;
+    vec3 foreGround = white;
+    
+    
+    //   implement the trailing effectm using the alpha channel to track the state of decay 
+    if(condition){
+        if(lastFeedback < 1.1) {
+            feedback = 1.;
+            cc = trail; 
+        } 
+        // else {
+        //     feedback = lastFeedback * decay;
+        //     c = mix(snap, bb, lastFeedback);
+        // }
+    }
+    else {
+        feedback = lastFeedback * decay;
+        if(lastFeedback > 0.4) {
+            cc = mix(foreGround, trail, lastFeedback); 
+        } else {
+            feedback = 0.;
+            cc = foreGround;
+        }
+    }
+    
+    
+    gl_FragColor = vec4(cc, feedback);
+}
+
